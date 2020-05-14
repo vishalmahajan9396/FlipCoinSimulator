5705d4aff434560f8746f165fa6074a0e6732216
vishalmahajan9396 <vshalmahajan9396@gmail.com>
2020-05-14 10:08:08 +0530

    [Vishal]Updated:Show Number of time Head and Tail Won

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -3,11 +3,22 @@
 echo "Welcome to Flip Coin Simulation";
 
 isHead=1;
-randomCheck=$(( RANDOM % 2 ));
+isHeadWon=0;
+isTailWon=0;
 
-if [ $isHead -eq $randomCheck ]
-then
-	echo "Head";
-else
-	echo "Tail";
-fi
+for (( count=0; count<=20; count++ ))
+do
+	randomCheck=$(( RANDOM % 2 ));
+
+	if [ $isHead -eq $randomCheck ]
+	then
+		echo "Head";
+		isHeadWon=$(( isHeadWon + 1 ));
+	else
+		echo "Tail";
+		isTailWon=$(( isTailWon + 1 ));
+	fi
+done
+
+echo "Total Number Head Won:" $isHeadWon;
+echo "Total Number Tail Won:" $isTailWon;

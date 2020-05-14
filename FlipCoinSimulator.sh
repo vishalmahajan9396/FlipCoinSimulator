acc4e023c187f0884887bb90bfea4599227c26ef
vishalmahajan9396 <vshalmahajan9396@gmail.com>
2020-05-14 11:29:42 +0530

    [Vishal]Updated:If it's Tie then continues till difference of minimum two points is achieved

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -5,6 +5,7 @@ echo "Welcome to Flip Coin Simulation";
 isHead=1;
 isHeadWon=0;
 isTailWon=0;
+winDifference=0;
 
 while [[ $isHeadWon -lt 21 && $isTailWon -lt 21 ]]
 do
@@ -13,10 +14,10 @@ do
 	if [ $isHead -eq $randomCheck ]
 	then
 		echo "Head";
-		isHeadWon=$(( isHeadWon + 1 ));
+		isHeadWon=$(( $isHeadWon + 1 ));
 	else
 		echo "Tail";
-		isTailWon=$(( isTailWon + 1 ));
+		isTailWon=$(( $isTailWon + 1 ));
 	fi
 done
 
@@ -25,12 +26,26 @@ echo "Total Number Tail Won:" $isTailWon;
 
 if [[ $isHeadWon -gt $isTailWon ]]
 then
-	difference=$(($isHeadWon - $isTailWon ));
-	echo "Head Wins" $difference "times more than Tail";
+	echo "Head Wins";
 elif [[ $isHead -eq $isTailWon ]]
 then
 	echo "Tie between Head and Tail";
+	while [[ $winDifference -lt 2 ]]
+	do
+		 randomCheck=$(( RANDOM % 2 ));
+
+	   if [ $isHead -eq $randomCheck ]
+   	then
+      	echo "Head";
+      	isHeadWon=$(( $isHeadWon + 1 ));
+			winDifference=$(( $winDifference + 1 ));
+   	else
+      	echo "Tail";
+      	isTailWon=$(( $isTailWon + 1 ));
+			winDifference=$(( $winDifference + 1 ));
+   	fi
+	done
+
 else
-	difference=$(($isTailWon - $isHeadWon ));
-	echo "Tail Wins" $difference "times more than Head";
+	echo "Tail Wins";
 fi
